e3a4b361b4f0554d81876241f8a491f7768fc316
SmellyNS <dmitryl5311@ya.ru>
2019-11-30 02:52:21 +0300

    lab04 uploaded

--- lab04/CLR.sql
+++ lab04/CLR.sql
@@ -31,18 +31,91 @@ CREATE ASSEMBLY Mult FROM 'C:\Users\GTAO\git\Databases\lab04\cs\Mult.dll';
 GO
 CREATE AGGREGATE Mult (@input int) RETURNS int 
 EXTERNAL NAME Mult.Mult;  
-
+ 
+SELECT dbo.Mult(A.Sram) from (select TOP(5) Sram from S) as A;  
 
 drop aggregate Mult
 drop assembly Mult
+go
 
-CREATE TYPE MyTableType AS table (ItemValue int);  
-go  
+-- Table CLR
 
-DECLARE @myTable AS MyTableType;  
-  
-INSERT INTO @myTable values(2),(3),(4);  
-  
-SELECT dbo.Mult(A.Sram) from (select Sram from S) as A;  
-go  
-select Sid from S where Sram = 0
+CREATE ASSEMBLY interval FROM 'C:\Users\GTAO\git\Databases\lab04\cs\table.dll';  
+GO
+
+create function dbo.interval(@a int)
+returns table (ID int)
+as
+external name interval.[pow2.TableValuedFunction].GenerateInterval
+go
+
+select * from dbo.interval(20)
+
+drop function interval
+drop assembly interval
+
+
+
+-- proc CLR
+
+CREATE ASSEMBLY sumramproc FROM 'C:\Users\GTAO\git\Databases\lab04\cs\proc.dll';  
+GO
+
+CREATE PROCEDURE RamSum (@sum int OUTPUT)  
+AS EXTERNAL NAME sumramproc.StoredProcedures.RamSum
+Go
+
+declare @rets int
+exec RamSum @rets output
+select @rets
+
+
+-- trigger CLR
+
+
+CREATE ASSEMBLY sometrigger FROM 'C:\Users\GTAO\git\Databases\lab04\cs\trigger.dll';  
+GO
+
+
+create trigger think on U instead of delete as
+EXTERNAL NAME sometrigger.CLRTriggers.DropTableTrigger
+go
+
+
+delete from U where Uid = 1
+
+select * from U where Uid = 1
+
+
+drop trigger think
+drop assembly sometrigger
+
+
+
+-- type CLR
+
+CREATE ASSEMBLY point FROM 'C:\Users\GTAO\git\Databases\lab04\cs\type.dll';  
+GO
+
+CREATE TYPE Point   
+EXTERNAL NAME point.[Point];  
+
+
+
+
+-- type test
+CREATE TABLE dbo.Points 
+( 
+  id INT IDENTITY(1,1) NOT NULL, 
+  point Point NULL 
+);
+GO
+
+INSERT INTO dbo.Points(point) 
+VALUES('0,0'),
+	  ('1,1'),
+	  ('1,5'),
+	  ('5,6')
+GO
+
+select point.ToString() from Points
---
+++ lab04/cs/proc.cs
@@ -0,0 +1,27 @@
+using System;  
+using System.Data.SqlTypes;  
+using System.Data.SqlClient;  
+using Microsoft.SqlServer.Server;   
+  
+public class StoredProcedures   
+{  
+   [Microsoft.SqlServer.Server.SqlProcedure]  
+   public static void RamSum(out SqlInt32 value)  
+   {  
+      using(SqlConnection connection = new SqlConnection("context connection=true"))   
+      {  
+         value = 0;  
+         connection.Open();  
+         SqlCommand command = new SqlCommand("SELECT Sram FROM S", connection);  
+         SqlDataReader reader = command.ExecuteReader();  
+  
+         using (reader)  
+         {  
+            while( reader.Read() )  
+            {  
+               value += reader.GetSqlInt32(0);  
+            }  
+         }           
+      }  
+   }  
+}  
